ac5c698514356923cdc1af003431b5ff840e19d0
Jerson Mateo Mendoza Melo <jerson.mendoza570@pascualbravo.edu.co>
2026-08-27 16:47:43 -0500

    frmAcercaDe completo

--- Pantalla_Sistema_Facturacion/frmAcercade.Designer.cs
+++ Pantalla_Sistema_Facturacion/frmAcercade.Designer.cs
@@ -28,19 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            panel1 = new Panel();
+            label5 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(168, 140);
+            label1.Name = "label1";
+            label1.Size = new Size(190, 19);
+            label1.TabIndex = 0;
+            label1.Text = "Jerson Mendoza Melo";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(118, 191);
+            label2.Name = "label2";
+            label2.Size = new Size(285, 19);
+            label2.TabIndex = 0;
+            label2.Text = "Herramientas de Programacion III";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(104, 250);
+            label3.Name = "label3";
+            label3.Size = new Size(314, 19);
+            label3.TabIndex = 0;
+            label3.Text = "Institucion Universitaria Pascual Bravo";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(218, 412);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 19);
+            label4.TabIndex = 0;
+            label4.Text = "V 1.0.0";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Highlight;
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(532, 67);
+            panel1.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Century Gothic", 13.2000008F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.Control;
+            label5.Location = new Point(202, 23);
+            label5.Name = "label5";
+            label5.Size = new Size(133, 27);
+            label5.TabIndex = 1;
+            label5.Text = "Acerca De";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.logo;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Dock = DockStyle.Left;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(93, 67);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
             // frmAcercade
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(532, 450);
+            Controls.Add(panel1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "frmAcercade";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "frmAcercade";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Panel panel1;
+        private Label label5;
+        private PictureBox pictureBox1;
     }
 }
